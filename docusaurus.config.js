f1aa4121d7e4b6a78963b507026458ce39e39f17
Caleb Figgers <cdfiggers@gmail.com>
2023-02-16 21:53:08 -0600

    Social card fix(?)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,7 +110,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/calebsnotes-social-card.png',
       docs: {
         sidebar: {
           hideable: true,
